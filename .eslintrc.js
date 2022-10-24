f3d1135881e66b217282a50740ab12fd54c5938e
Chun-igaw <Chun.lee@igaworks.com>
2022-10-24 10:26:23 +0900

    feat: #1 프로젝트 세팅 - eslint rules 세팅 - 라이브러리 설치 (vuex-module-decorators 등)

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,19 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
+    'eslint:recommended',
+    '@vue/typescript/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    indent: ['error', 2], // tab size 2
+    'object-curly-spacing': ['error', 'always'], // 중괄호 내 공백
+    semi: [2, 'always'], // 세미콜론
+    quotes: ['error', 'single', { allowTemplateLiterals: true }], // 싱글쿼트, 템플릿리터럴 허용,
+    'jsx-quotes': ['error', 'prefer-double'], // jsx 내에서는 더블쿼트
   }
-}
+};
