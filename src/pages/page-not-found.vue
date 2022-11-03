b95e415a7452ef2aca8d3a8a5e7a473b0060af6b
Chun-igaw <Chun.lee@igaworks.com>
2022-11-03 21:53:32 +0900

    feat: #48 not found 페이지 스타일링

--- src/pages/page-not-found.vue
+++ src/pages/page-not-found.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="not-found">
-    <div class="not-found--title">404 Not Found</div>
-    <div class="not-found--title-secondary"><router-link :to="{ path: '/'}">메인으로 돌아가기</router-link></div>
+    <div class="not-found__title">404 Not Found</div>
+    <div class="not-found__description">잘못된 페이지 접근입니다</div>
+    <div class="not-found__a--goBack"><router-link :to="{ path: '/'}">메인으로 돌아가기</router-link></div>
   </div>
 </template>
 
