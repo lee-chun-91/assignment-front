528013d08d807ef0fec5d9237926cd71e4ea8ad8
Chun-igaw <Chun.lee@igaworks.com>
2022-11-04 11:57:37 +0900

    fix: #65 로그인 시 라우팅 name 수정

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,6 @@
 
 
+// date 값 변환
 export const convertDate = (date: Date) => {
   const YY = date.getFullYear();
   const MM = date.getMonth() + 1;
@@ -9,24 +10,27 @@ export const convertDate = (date: Date) => {
   return YY + '-' + MM + '-' + DD + ' ' + hh + ':' + mm;
 };
 
+
+// date 값 같은 날인지 비교
 export const isSameDate = (date1: Date, date2: Date) => {
   return date1.getFullYear() === date2.getFullYear()
     && date1.getMonth() === date2.getMonth()
     && date1.getDate() === date2.getDate();
 };
 
-//
+
+// chart.js 에서 요구하는 data 모양에 맞추어 변환해주는 함수
 const init:{ [index:string]: number } = {};
 export const getElementCount = (arr: string[]) => arr.reduce((ac, v) => ({ ...ac, [v]: (ac[v] || 0) + 1 }), init);
 
 
-//
+// random rgb 값을 반환해주는 함수
 export const getRandomColor = () =>
   `rgb( ${new Array(3).fill('')
     .map(v => Math.floor(Math.random() * 256)).join(', ')} )`;
 
 
-//
+// uuid 를 생성해주는 함수
 export const uuid = () => {
   let uuidValue = '', k, randomValue;
   for (k = 0; k < 32;k++) {
